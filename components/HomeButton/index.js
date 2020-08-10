22b4ae1f5fdc053a9a5f606926855575a397f725
DiegoOTdC <studioartdiego@gmail.com>
2020-08-10 18:42:42 +0200

    add component/HomeButton for styling buttons on homeScreen

---
+++ components/HomeButton/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, TouchableWithoutFeedback, View, Text } from "react-native";
+
+export default function HomeButton({ title, onPress, backgroundColor }) {
+  console.log(backgroundColor);
+  return (
+    <TouchableWithoutFeedback
+      onPressIn={(e) => {
+        console.log("user pressed my button");
+      }}
+      onPressOut={(e) => {
+        console.log("user released my button");
+      }}
+      onPress={onPress}
+    >
+      <View
+        style={{
+          height: "50%",
+          width: "100%",
+
+          backgroundColor: backgroundColor,
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            top: "35%",
+            color: "black",
+            fontSize: 18,
+            fontWeight: "bold",
+          }}
+        >
+          {title}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
+import HomeButton from "../../components/HomeButton";
 
 export default function HomeScreen() {
   return (
-    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-      <Text
-        style={{
-          fontWeight: "bold",
-          fontSize: 30,
-          textAlign: "center",
-        }}
-      >
-        Welcome to my awesome app!
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+      }}
+    >
+      <HomeButton
+        style={{ justifyContent: "top" }}
+        title="Scan Image"
+        onPress=""
+        backgroundColor="#b3d89cff"
+      />
+
+      <HomeButton
+        style={{ justifyContent: "top" }}
+        title="Scan Barcode"
+        onPress=""
+        backgroundColor="#3b7080ff"
+      />
     </View>
   );
 }

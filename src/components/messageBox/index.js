d7cceac978c64af80a613a2cb2482dc434d78b4e
DiegoOTdC <studioartdiego@gmail.com>
2020-08-20 18:08:37 +0200

    setup messageBox component

---
+++ src/components/messageBox/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { View, Text } from "react-native";
+
+export default function MessageBox() {
+  return (
+    <View>
+      <Text>MessageBox</Text>
+    </View>
+  );
+}

aa014e618992b537f4e3e1d1e991dec507e415aa
DiegoOTdC <studioartdiego@gmail.com>
2020-08-20 19:22:03 +0200

    finish MessageBox component, remove 'dismissable' from appState/actions

--- src/components/messageBox/index.js
+++ src/components/messageBox/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { selectMessage } from "../../store/appState/selectors";
 import { View, Text, ActivityIndicator } from "react-native";
 import { useFonts, Alata_400Regular } from "@expo-google-fonts/alata";
+import { red, green, blue } from "../../colours";
 
 export default function MessageBox() {
+  const message = useSelector(selectMessage);
+  const showMessage = message !== null;
+  if (!showMessage) return null;
+
   const [fontsLoaded] = useFonts({
     Alata_400Regular,
   });
@@ -12,21 +18,53 @@ export default function MessageBox() {
     return <ActivityIndicator />;
   }
 
-  const message = "useSelector(selectMessage)";
-  const showMessage = message !== null;
-  if (!showMessage) return null;
+  const backgroundColor = () => {
+    if (message.variant === "success") {
+      return green;
+    }
+
+    if (message.variant === "danger") {
+      return red;
+    }
+
+    if (message.variant === "warning") {
+      return blue;
+    }
+  };
+
+  const color = () => {
+    if (showMessage.variant === "success") {
+      return blue;
+    }
+
+    if (showMessage.variant === "danger") {
+      return green;
+    }
+
+    if (showMessage.variant === "warning") {
+      return red;
+    }
+  };
 
   return (
-    <View style={{ flex: 0.1, height: 10, justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 0.1,
+        height: 10,
+        justifyContent: "center",
+        backgroundColor: { backgroundColor },
+      }}
+    >
       <Text
         numberOfLines={2}
         style={{
           textAlign: "center",
           fontFamily: "Alata_400Regular",
+          color: { color },
           fontSize: 20,
         }}
       >
-        {message}
+        {showMessage.text}
       </Text>
     </View>
   );
--- src/store/appState/actions.js
+++ src/store/appState/actions.js
@@ -7,25 +7,19 @@ export const appLoading = () => ({ type: APP_LOADING });
 export const appDoneLoading = () => ({ type: APP_DONE_LOADING });
 export const clearMessage = () => ({ type: CLEAR_MESSAGE });
 
-export const setMessage = (variant, dismissable, text) => {
+export const setMessage = (variant, text) => {
   return {
     type: SET_MESSAGE,
     payload: {
       variant,
-      dismissable,
       text,
     },
   };
 };
 
-export const showMessageWithTimeout = (
-  variant,
-  dismissable,
-  text,
-  timeOutMilliSeconds
-) => {
+export const showMessageWithTimeout = (variant, text, timeOutMilliSeconds) => {
   return (dispatch) => {
-    dispatch(setMessage(variant, dismissable, text));
+    dispatch(setMessage(variant, text));
 
     const timeout = timeOutMilliSeconds || 2000;
 
